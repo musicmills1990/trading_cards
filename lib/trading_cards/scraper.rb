ae7b7186efa72be2fa0c1adb6f603f0f17d73320
Marc Mills <mdmills1990@gmail.com>
2018-10-10 00:48:59 +0000

    some more scraping, verge of figuring out wtf I'm doing!

--- lib/trading_cards/scraper.rb
+++ lib/trading_cards/scraper.rb
@@ -40,3 +40,69 @@ attr_accessor :edition, :doc
 
 
 end
+
+
+#since I've got gemspec problems and console problems, I figure out some scraping in repl.it. Here's that work:
+
+#d = Nokogiri::HTML(open("https://musicthegathering.com"))
+
+#mtg = d.search("div#block-yui_3_17_2_1_1534896694126_49243.sqs-block.html-block.sqs-block-html").text.strip
+
+#craic = d.search("div#block-yui_3_17_2_1_1534896694126_56732.sqs-block.html-block.sqs-block-html").text.strip
+
+#chaste = d.search("div#block-yui_3_17_2_1_1534896694126_62410.sqs-block.html-block.sqs-block-html").text.strip.gsub("NEW! ", "")
+
+#plunder = d.search("div#block-yui_3_17_2_1_1534943478376_19237.sqs-block.html-block.sqs-block-html").text.strip.gsub("NEW! ", "")
+
+#lady_v = d.search("div#block-yui_3_17_2_1_1534943478376_60459.sqs-block.html-block.sqs-block-html").text.strip.gsub("NEW! ", "")
+
+#editions = [mtg, craic, chaste, plunder, lady_v]
+
+#editions.each_with_index do |edition, i|
+#puts "#{i+1}. #{edition}"
+#end
+
+#need to figure out how to pull the URLs
+#https://www.musicthegathering.com/music-the-gathering-cards/
+#https://www.musicthegathering.com/the-craic-edition-cards/
+#https://www.musicthegathering.com/chaste-treasure-edition-cards/
+#https://www.musicthegathering.com/the-plunder-doggs-edition-cards
+#https://www.musicthegathering.com/the-lady-victoria-card
+
+
+
+#mtg_doc = Nokogiri::HTML(open("https://musicthegathering.com/music-the-gathering-cards"))
+
+#MTG characters
+#azia = mtg_doc.search("div#block-yui_3_17_2_1_1534892891091_37329.sqs-block.html-block.sqs-block-html").each do |character_div|
+#azia_name = character_div.search("h3").text.strip
+#azia_description = character_div.search("p").text.strip
+#puts azia_name
+#puts ""
+#puts azia_description
+#puts ""
+#end
+
+
+
+#liv = mtg_doc.search("div#block-yui_3_17_2_1_1534892891091_38648.sqs-block.html-block.sqs-block-html").each do |character_div|
+#liv_name = character_div.search("h3").text
+#liv_description = character_div.search("p").text
+#puts liv_name
+#puts ""
+#puts liv_description
+#puts ""
+#end
+
+
+#marco = mtg_doc.search("div#block-yui_3_17_2_1_1534892891091_40394.sqs-block.html-block.sqs-block-html").each do |character_div|
+#marco_name = character_div.search("h3").text
+#marco_description = character_div.search("p").text
+#puts marco_name
+#puts ""
+#puts marco_description
+#puts ""
+#end
+
+
+
