66503f40d93f7f705881edc3292d94516bb88022
Marc Mills <mdmills1990@gmail.com>
2018-10-16 23:47:37 +0000

    arbitrary semantic fix

--- lib/trading_cards/edition.rb
+++ lib/trading_cards/edition.rb
@@ -29,7 +29,7 @@ class Edition
 
     def self.scrape_mtg
       Edition.new.tap do |e|
-        Edition.doc.search("#music-the-gathering-cards-page").collect do |a|
+        self.doc.search("#music-the-gathering-cards-page").collect do |a|
           e.name = a.search("div#block-f11ba348dcafc96ca707.sqs-block.html-block.sqs-block-html h2").text
           e.characters = a.search("div.col.sqs-col-4.span-4 h3").to_a.join(", ")
           e.description = a.search("div.col.sqs-col-4.span-4 p").to_a.join(" ")
@@ -40,7 +40,7 @@ class Edition
 
     def self.scrape_craic
       Edition.new.tap do |e|
-        Edition.doc.css("#the-craic-edition-cards-page").collect do |a|
+        self.doc.css("#the-craic-edition-cards-page").collect do |a|
           e.name = a.search("h2").text.strip.gsub("Buy All Four!", "")
           e.characters = a.search("div.col.sqs-col-6.span-6 h3").to_a.join(", ")
           e.description = a.search("div.col.sqs-col-6.span-6 p").to_a.join(" ")
@@ -50,7 +50,7 @@ class Edition
 
     def self.scrape_chaste
       Edition.new.tap do |e|
-        Edition.doc.css("#chaste-treasure-edition-cards-page").collect do |a|
+        self.doc.css("#chaste-treasure-edition-cards-page").collect do |a|
           e.name = a.search("div.col.sqs-col-12.span-12 h2").text.strip
           e.characters = a.search("div.col.sqs-col-4.span-4 h3").to_a.join(", ")
           e.description = a.search("div.col.sqs-col-4.span-4 p").to_a.join(" ")
@@ -60,7 +60,7 @@ class Edition
 
     def self.scrape_plunder
       Edition.new.tap do |e|
-        Edition.doc.css("#the-plunder-doggs-edition-cards-page").collect do |a|
+        self.doc.css("#the-plunder-doggs-edition-cards-page").collect do |a|
           e.name = a.search("div.sqs-block-content h2").text.strip
           e.characters = a.search("div.col.sqs-col-6.span-6 h3").to_a.join(", ")
           e.description = a.search("div.col.sqs-col-6.span-6 p").to_a.join(" ")
@@ -70,7 +70,7 @@ class Edition
 
     def self.scrape_lady_v
       Edition.new.tap do |e|
-        Edition.doc.css("#the-lady-victoria-card-page").collect do |a|
+        self.doc.css("#the-lady-victoria-card-page").collect do |a|
           e.name = a.search("h2").text.strip
           e.characters = a.search("h3").text.strip
           e.description = a.search("div#block-yui_3_17_2_1_1534946050355_21337 p").text.strip
